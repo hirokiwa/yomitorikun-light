dafa4bd06057461db0fa001785bb6d7aaa900cc4
hirokiwa <hirokifsys@gmail.com>
2026-07-06 22:46:25 +0900

    Use transformIndexHtml to insert Google Analytics tag

--- build/vite-plugins/googleAnalytics.ts
+++ build/vite-plugins/googleAnalytics.ts
@@ -1,17 +1,31 @@
 import type { Plugin } from 'vite';
 
 const measurementId = 'G-JWLNCNBNLF';
-const placeholder = '<meta data-build-placeholder="google-analytics">';
-const googleAnalyticsTag = `<script async src="https://www.googletagmanager.com/gtag/js?id=${measurementId}"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag() { dataLayer.push(arguments); }
-  gtag('js', new Date());
-  gtag('config', '${measurementId}');
-</script>`;
 
 export const googleAnalyticsPlugin: Plugin = {
   name: 'google-analytics',
   apply: 'build',
-  transformIndexHtml: (html) => html.replace(placeholder, googleAnalyticsTag),
+  transformIndexHtml: (html) => ({
+    html,
+    tags: [
+      {
+        tag: 'script',
+        attrs: {
+          async: true,
+          src: `https://www.googletagmanager.com/gtag/js?id=${measurementId}`,
+        },
+        injectTo: 'head',
+      },
+      {
+        tag: 'script',
+        children: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag() { dataLayer.push(arguments); }
+          gtag('js', new Date());
+          gtag('config', '${measurementId}');
+        `.trim(),
+        injectTo: 'head',
+      },
+    ],
+  }),
 };
